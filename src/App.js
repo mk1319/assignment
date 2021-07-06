e87d134dc5dc7dad0209ae89ea8c36bd755b5319
Mukesh Rajpurohit <mukeshrr1321@outlook.com>
2021-07-06 17:02:27 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import JsonFile from './moviedata.json'
+
+
+
+const Rating=({rate})=>{
+  return [...Array(5).keys()].map((e,index)=>
+    { return e<rate?<li style={{color:'yellow'}} key={index}>&#9733;</li>:<li key={index} style={{color:'gray'}}>&#9733;</li>
+    })
+}
+
+
+const Card=({data})=>{
+  return(
+    <div className="moviecard">
+      <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}/>
+        <h1>{data.original_title}</h1>
+        <ul className="rate">
+        <Rating rate={data.vote_average>0?data.vote_average/2:data.vote_average}/>
+        </ul>
+        <p className="description">{data.overview}</p>
+        <p>release_date: {data.release_date}</p>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="movielist">
+      {
+        Array.isArray(JsonFile)&&JsonFile.map((e,index)=><Card data={e} key={index}/>)
+      }
+      </div>
     </div>
   );
 }
